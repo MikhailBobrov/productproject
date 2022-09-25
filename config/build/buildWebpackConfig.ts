ed93e17f33deaa665e31c626162b463c876735c4
MikhailBobrov <mikhailurban13@yandex.ru>
2022-09-25 19:49:05 +0500

    css-modules изоляция стилей и  MiniCssExtractPlugin

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,5 +1,4 @@
 import webpack from "webpack";
-import path from "path";
 import { BuildOptions } from "./types/config";
 import { buildLoaders } from "./buildLoaders";
 import { buildPlugins } from "./buildPlugins";
@@ -20,7 +19,7 @@ export function buildWebpackConfig(
     },
     plugins: buildPlugins(options),
     module: {
-      rules: buildLoaders(),
+      rules: buildLoaders(options),
     },
     resolve: buildResolvers(),
     devtool: isDev ? "inline-source-map" : undefined,
